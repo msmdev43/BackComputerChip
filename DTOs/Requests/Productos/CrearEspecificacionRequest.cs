37bebb7e835da852d74804a3d7ed49fbcff3940f
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-06-25 14:07:42 -0300

    feat(request): Crear nueva clase para crear especificaciones con sus atributos.

---
+++ DTOs/Requests/Productos/CrearEspecificacionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerChip.DTOs.Requests.Productos
+{
+    public class CrearEspecificacionRequest
+    {
+        public string titulo { get; set; } = string.Empty;
+
+        public string descripcion { get; set; } = string.Empty;
+
+    }
+}
