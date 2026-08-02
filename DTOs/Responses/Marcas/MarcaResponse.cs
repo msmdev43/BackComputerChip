18c1059a74ffa42024075e8a257345289ba07767
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-08-02 16:03:49 -0300

    feat(MarcasResponse): Crear DTO para la respuesta de Marca
    - Agregar atributos basicos para mostrar de la entidad Marca

---
+++ DTOs/Responses/Marcas/MarcaResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerChip.DTOs.Responses.Marcas
+{
+    public class MarcaResponse
+    {
+        public string nombre { get; set; } = string.Empty;
+    }
+}
