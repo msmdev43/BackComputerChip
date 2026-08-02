0af08c7d7594dcc1f79282ac95b5e0b72eef66ba
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-08-02 16:01:00 -0300

    feat(MarcasRequest): Crear DTO de request para Marca
    - Agrega atributo simples en la creacion de marca

---
+++ DTOs/Requests/Marcas/CrearMarcaRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerChip.DTOs.Requests.Marcas
+{
+    public class CrearMarcaRequest
+    {
+        [Required (ErrorMessage = "El nombre de la marca es obligatorio")]
+        [MaxLength(105, ErrorMessage = "El nombre de la marca no puede superar los 105 caracteres")]
+        public string nombre { get; set; } = string.Empty;
+
+    }
+}
