62527cf880fdf3bfadd9d0143146d69ca4b27e25
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-08-02 15:43:37 -0300

    feat(UsuarioReponse): Crear DTO de respuesta para usuario
    - Incluye todos los campos del modelo Usuario (sin password y emailVerify)
    - Excluye datos vulnerables como tokens y logins externos

---
+++ DTOs/Responses/Usuarios/UsuarioResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerChip.DTOs.Responses.Usuarios
+{
+    public class UsuarioResponse
+    {
+        public int Id { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Email { get; set; }
+        public string? Pais { get; set; }
+        public string? Provincia { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Calle { get; set; }
+        public string? Numero { get; set; }
+        public string? Celular { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
