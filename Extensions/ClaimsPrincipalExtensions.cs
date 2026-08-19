e53db0ba418b0221e064e98e7fa3e70fa5711dc8
Nahuel Zubiri <nahuelzubiri@hotmail.es>
2026-08-19 14:56:22 -0300

    feat(extensions): agregar ClaimsPrincipalExtensions para obtener ID del usuario autenticado

---
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerChip.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                              ?? user.FindFirst("sub")?.Value
+                              ?? user.FindFirst("userId")?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim))
+                throw new UnauthorizedAccessException("Usuario no autenticado");
+
+            return int.Parse(userIdClaim);
+        }
+    }
+}
